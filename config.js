59832043a8a477f63e03bfac7895b5ea25696e60
dat231296-coder <dat231296@gmail.com>
2026-06-25 10:46:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // URL /exec của Apps Script Web App sau khi deploy.
 // Ví dụ: https://script.google.com/macros/s/AKfycb.../exec
-window.AI_ACADEMY_API_URL = 'PASTE_APPS_SCRIPT_WEB_APP_URL_HERE';
+window.AI_ACADEMY_API_URL = 'https://script.google.com/macros/s/AKfycbzPVCTqvEp55dgd_pTWY5g325yu8lK15-k3dB9AunnuoYYgbvS4D-m1lOHqvLBOYFS3/exec';
